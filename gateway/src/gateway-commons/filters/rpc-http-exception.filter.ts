5ae657710e3353c02f21016842c864aa9020320a
Bibek Koirala <bibek.koirala@ebpearls.com>
2025-11-05 16:47:59 +0545

    so what is this ?

--- gateway/src/gateway-commons/filters/rpc-http-exception.filter.ts
+++ gateway/src/gateway-commons/filters/rpc-http-exception.filter.ts
@@ -4,23 +4,28 @@ import { RpcException } from '@nestjs/microservices';
 @Catch()
 export class RpcHttpExceptionFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
-
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
+    console.log('exception', exception);
+
     if (exception instanceof RpcException) {
-      console.log("here in expception")
+      console.log('here in expception');
       return response.status(400).json({
+        success: false,
         status: 'error',
         message: exception.getError(),
       });
     }
 
-    console.log("done")
+    console.log('done');
 
     return response.status(500).json({
+      success: false,
       status: 'error',
       message: 'Internal server error',
     });
   }
 }
+
+// in async processing, DLQ and final format is used. while for sync processing, crafted response is given and DLQ Is involved.
--- users/src/users/users.service.ts
+++ users/src/users/users.service.ts
@@ -6,37 +6,49 @@ import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { RpcException } from '@nestjs/microservices';
 
-
-
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, private jwtService: JwtService,) { }
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
   async findAll(): Promise<UserDocument[]> {
-    const result = await this.userModel.find().exec()
-    return result
+    const result = await this.userModel.find().exec();
+    return result;
   }
 
   async findById(id: string): Promise<UserDocument> {
     const user = await this.userModel.findById(id).exec();
-    console.log("userr in findById", user)
+    console.log('userr in findById', user);
     if (!user) {
-      throw new RpcException("User Not Found")
+      throw new RpcException('User Not Found');
     }
     return user;
   }
 
-  async register(name: string, email: string, password: string): Promise<UserDocument> {
+  async register(
+    name: string,
+    email: string,
+    password: string,
+  ): Promise<UserDocument> {
     const exists = await this.userModel.findOne({ email });
     if (exists) throw new RpcException('Email already in use');
 
     const hashed = await bcrypt.hash(password, 10);
 
-    const createdUser = await this.userModel.create({ name, email, password: hashed });
-    return createdUser
+    const createdUser = await this.userModel.create({
+      name,
+      email,
+      password: hashed,
+    });
+    return createdUser;
   }
 
-  async login(email: string, password: string): Promise<{ accessToken: string; user: UserDocument }> {
+  async login(
+    email: string,
+    password: string,
+  ): Promise<{ accessToken: string; user: UserDocument }> {
     const user = await this.userModel.findOne({ email });
     if (!user) throw new RpcException('User not found');
 
@@ -50,9 +62,11 @@ export class UsersService {
   }
 
   async update(id: string, data: Partial<UserDocument>): Promise<UserDocument> {
-    const updatedUser = await this.userModel.findByIdAndUpdate(id, data, { new: true }).exec();
+    const updatedUser = await this.userModel
+      .findByIdAndUpdate(id, data, { new: true })
+      .exec();
     if (!updatedUser) {
-      throw new RpcException("No user found to update")
+      throw new RpcException('No user found to update');
     }
     return updatedUser;
   }
@@ -60,7 +74,7 @@ export class UsersService {
   async delete(id: string): Promise<UserDocument> {
     const deletedUser = await this.userModel.findByIdAndDelete(id).exec();
     if (!deletedUser) {
-      throw new RpcException("No user to delete")
+      throw new RpcException('No user to delete');
     }
     return deletedUser;
   }
